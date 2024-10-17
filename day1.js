7262723a413402510e72cc5495b1b24c0972d21e
NayanChoudhari <136890339+nayanchoudhari255@users.noreply.github.com>
2024-10-17 14:42:54 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,18 @@
+ console.log("vamsi");
+ console.log("ap");
+console.log(12345);
+ 
+ var a1=10;
+ var b1=20;
+console.log(a1);
+ console.log(b1);
+
+var name="nayan";
+var lastname="choudhari";
+console.log(name);
+console.log(lastname);
+
+var a=30;
+var b=40;
+console.log(a);
+console.log(b);
